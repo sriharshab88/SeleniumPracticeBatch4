9d8ccc36d99e17c6791334b4d3b5484982103bea
Mohan S <mohan.s@excelindia.com>
2018-03-31 08:55:03 +0530

    [Mohan]created a method for pul request. Now committing.

--- src/main/java/com/harsha/seleniumPractice/webDriver/sampleTestScripts/mohanclass.java
+++ src/main/java/com/harsha/seleniumPractice/webDriver/sampleTestScripts/mohanclass.java
@@ -1,5 +1,11 @@
 package com.harsha.seleniumPractice.webDriver.sampleTestScripts;
 
+import org.testng.annotations.Test;
+
 public class mohanclass {
 
+	@Test
+	public void testMethod(){
+		System.out.println("Created Method for Pull reqest");
+	}
 }
