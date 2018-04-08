9eae3181dc104886dcffc1a9f9897cdc71b36928
Harsha <teamindia#456>
2018-04-08 07:39:18 +0530

    [Harsha] Added Genericmethods and modified test scripts

--- src/main/java/com/harsha/seleniumPractice/libraries/Configurations.java
+++ src/main/java/com/harsha/seleniumPractice/libraries/Configurations.java
@@ -2,6 +2,11 @@ package com.harsha.seleniumPractice.libraries;
 
 import java.io.File;
 
+/**
+ * This class file contains all the pre-requisites required to run the automation scripts
+ * @author Sri harsha
+ *
+ */
 public class Configurations {
 
 	public static String applicationUrl = "http://automationpractice.com/index.php";
